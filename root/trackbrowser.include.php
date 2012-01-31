2008fdb244c0c54a25d60b23d2c13c01ccb9d41b
Rob <rob@mintsoft.net>
2012-01-31 19:03:50 +0000

    Implementing tweaks to layout ready for dynamic video pane display

--- root/trackbrowser.include.php
+++ root/trackbrowser.include.php
@@ -1,5 +1,9 @@
 <div class='containerPadding'>
-	<div id="jquery_jplayer_1"></div>
-	<ol id='tracklist' class='' >
-	</ol>
+	<div id='centerTrackContainer'>
+		<ol id='tracklist' class='' >
+		</ol>
+	</div>
+	<div id='centerPlayerContainer'>
+		<div id="jquery_jplayer_1"></div>
+	</div>
 </div>
