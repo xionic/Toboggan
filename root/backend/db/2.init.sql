d760fed1872c5d7b9e7a4747bdbf37279747a83b
Nick Spargo <intergalacticlollypopman@hotmail.com>
2014-04-17 17:11:01 +0100

    fix default video streamer

--- root/backend/db/2.init.sql
+++ root/backend/db/2.init.sql
@@ -1,9 +1,10 @@
 INSERT INTO schema_information(version) VALUES("104");
 
 --commands
-INSERT INTO Command(command, displayName) VALUES("ffmpeg -i %path -ab %bitrate -v 0 -f mp3 -", "Anything to mp3");
+INSERT INTO Command(command, displayName) VALUES("/usr/bin/ffmpeg -i %path -ab %bitrate -v 0 -f mp3 -", "Anything to mp3");
 INSERT INTO Command(command, displayName) VALUES("/usr/bin/ffmpeg -i %path 2>&1 | sed -n -e 's/.*bitrate: \([0-9]\+\).*/\1/p'", "Generic bitrate getter");
-INSERT INTO Command(command, displayName) VALUES("ffmpeg -i %path 2>&1 | sed -n -e 's/.*Duration: \([0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}\).*/\1/p' | awk -F ':' '{print ($1*3600)+($2*60)+$3}'", "Generic duration getter");
+INSERT INTO Command(command, displayName) VALUES("/usr/bin/ffmpeg -i %path 2>&1 | sed -n -e 's/.*Duration: \([0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}\).*/\1/p' | awk -F ':' '{print ($1*3600)+($2*60)+$3}'", "Generic duration getter");
+INSERT INTO Command(command, displayName) VALUES("/usr/bin/ffmpeg -ss %skipToTime -i %path -async 1 -b %bitrate -vf 'scale=320:trunc((320/a)/2)*2' -ar 44100 -ac 2 -v 0 -f flv -vcodec libx264 -preset superfast -", "Anything to avi");
 
 --file types
 INSERT INTO FileType(extension, mimeType, mediaType, idbitrateCmd, iddurationCmd) 
@@ -20,7 +21,7 @@ INSERT INTO FileType(extension, mimeType, mediaType, idbitrateCmd, iddurationCmd
 --file convertors
 INSERT INTO FileConverter(fromFileType, ToFileType, idcommand) VALUES("mp3","mp3",1);
 INSERT INTO FileConverter(fromFileType, ToFileType, idcommand) VALUES("wma","mp3",1);
-INSERT INTO FileConverter(fromFileType, ToFileType, idcommand) VALUES("avi","flv",1);
+INSERT INTO FileConverter(fromFileType, ToFileType, idcommand) VALUES("avi","flv",4);
 
 
 INSERT INTO mediaSource(path, displayName) VALUES("/mnt/storage/music", "Music");
