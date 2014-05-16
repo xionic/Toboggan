671622f11732ac54422159c92599b92b0d6b14e5
Nick Spargo <intergalacticlollypopman@hotmail.com>
2014-05-16 16:43:34 +0100

    switch to avconv command for default transcoders

--- root/backend/db/3.defaultdata.sql
+++ root/backend/db/3.defaultdata.sql
@@ -1,9 +1,9 @@
 
 --commands
-INSERT INTO Command(command, displayName) VALUES("/usr/bin/ffmpeg -ss %skipToTime -i %path -ab %bitrate -v 0 -f mp3 -", "Anything to mp3");
-INSERT INTO Command(command, displayName) VALUES("/usr/bin/ffmpeg -i %path 2>&1 | sed -n -e 's/.*bitrate: \([0-9]\+\).*/\1/p'", "Generic bitrate getter");
-INSERT INTO Command(command, displayName) VALUES("/usr/bin/ffmpeg -i %path 2>&1 | sed -n -e 's/.*Duration: \([0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}\).*/\1/p' | awk -F ':' '{print ($1*3600)+($2*60)+$3}'", "Generic duration getter");
-INSERT INTO Command(command, displayName) VALUES("/usr/bin/ffmpeg -ss %skipToTime -i %path -async 1 -b %bitrate -vf 'scale=320:trunc((320/a)/2)*2' -ar 44100 -ac 2 -v 0 -f flv -vcodec libx264 -preset superfast -", "Anything to flv");
+INSERT INTO Command(command, displayName) VALUES("/usr/bin/avconv -ss %skipToTime -i %path -ab %bitrate -v 0 -f mp3 -", "Anything to mp3");
+INSERT INTO Command(command, displayName) VALUES("/usr/bin/avconv -i %path 2>&1 | sed -n -e 's/.*bitrate: \([0-9]\+\).*/\1/p'", "Generic bitrate getter");
+INSERT INTO Command(command, displayName) VALUES("/usr/bin/avconv -i %path 2>&1 | sed -n -e 's/.*Duration: \([0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}\).*/\1/p' | awk -F ':' '{print ($1*3600)+($2*60)+$3}'", "Generic duration getter");
+INSERT INTO Command(command, displayName) VALUES("/usr/bin/avconv -ss %skipToTime -i %path -async 1 -b %bitrate -vf 'scale=320:trunc((320/a)/2)*2' -ar 44100 -ac 2 -v 0 -f flv -vcodec libx264 -preset superfast -", "Anything to flv");
 
 --file types
 INSERT INTO FileType(idfileType, extension, mimeType, mediaType, idbitrateCmd, iddurationCmd) 
