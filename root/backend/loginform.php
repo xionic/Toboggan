2a0855c54c1da0f2c348f6ea5c232820ce87f75b
Nick Spargo <intergalacticlollypopman@hotmail.com>
2014-11-07 15:33:25 +0000

    Remove login hack

--- root/backend/loginform.php
+++
@@ -1,5 +0,0 @@
-<form action="rest.php?action=login&apikey=testkey1&debug" method="POST">
-	<input name="username" value="testuser" />
-	<input name="password" value = "XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg="/>
-	<input type="submit" />
-</form>
