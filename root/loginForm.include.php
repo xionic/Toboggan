2962fde21d86c3a7bc4503b5eae6e6f41975749d
Nick Spargo <intergalacticlollypopman@hotmail.com>
2014-12-06 21:29:43 +0000

    Remove default creds

--- root/loginForm.include.php
+++ root/loginForm.include.php
@@ -1,5 +1,5 @@
 <form action='backend/rest.php?action=login' method='POST' id='loginForm'>
-	<p><label for='username'>username:</label><input type='text' id='username' name='username' value='testuser' /></p>
-	<p><label for='paswordInput'>password:</label><input type='password' id='passwordInput' name='' value='password'/></p>
+	<p><label for='username'>username:</label><input type='text' id='username' name='username' /></p>
+	<p><label for='paswordInput'>password:</label><input type='password' id='passwordInput' name='' /></p>
 	<p><input type='hidden' id='password' name='password' /></p>
 </form>
